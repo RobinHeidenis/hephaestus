e7a359f4547e5d11fd51319a26ba02c2093664e8
Robin Heidenis <robin.heidenis@gmail.com>
2024-04-01 02:06:57 +0100

    feat: 🎸 change bottom nav icon from outline to full on navigate

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import {Tabs} from 'expo-router';
-import {CommonActions} from "@react-navigation/native";
-import {getHeaderTitle} from "@react-navigation/elements";
-import {Appbar, BottomNavigation} from 'react-native-paper';
-import {MaterialCommunityIcons} from "@expo/vector-icons";
+import React from "react";
+import { Tabs } from "expo-router";
+import { CommonActions } from "@react-navigation/native";
+import { getHeaderTitle } from "@react-navigation/elements";
+import { Appbar, BottomNavigation } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function TabLayout() {
   return (
@@ -15,21 +14,21 @@ export default function TabLayout() {
           const title = getHeaderTitle(props.options, props.route.name);
 
           return (
-            <Appbar.Header style={{gap: 16, paddingHorizontal: 16}}>
-              <Appbar.Content title={title}/>
+            <Appbar.Header style={{ gap: 16, paddingHorizontal: 16 }}>
+              <Appbar.Content title={title} />
 
               {props.options.headerRight
                 ? props.options.headerRight({})
                 : undefined}
             </Appbar.Header>
-          )
+          );
         },
       }}
-      tabBar={({navigation, state, descriptors, insets}) => (
+      tabBar={({ navigation, state, descriptors, insets }) => (
         <BottomNavigation.Bar
           navigationState={state}
           safeAreaInsets={insets}
-          onTabPress={({route, preventDefault}) => {
+          onTabPress={({ route, preventDefault }) => {
             const event = navigation.emit({
               type: "tabPress",
               target: route.key,
@@ -45,22 +44,23 @@ export default function TabLayout() {
               });
             }
           }}
-          renderIcon={({route, focused, color}) => {
-            const {options} = descriptors[route.key];
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
             if (options.tabBarIcon) {
-              return options.tabBarIcon({focused, color, size: 24});
+              return options.tabBarIcon({ focused, color, size: 24 });
             }
 
             return null;
           }}
-          getLabelText={({route}) => {
-            const {options} = descriptors[route.key];
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
             const label =
-              options.tabBarLabel !== undefined && typeof options.tabBarLabel === 'string'
+              options.tabBarLabel !== undefined &&
+              typeof options.tabBarLabel === "string"
                 ? options.tabBarLabel
                 : options.title !== undefined
                   ? options.title
-                  : route.name ?? 'UHHHH';
+                  : route.name ?? "UHHHH";
 
             return label;
           }}
@@ -70,22 +70,34 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Links',
-          tabBarIcon: (props) => <MaterialCommunityIcons {...props} name={"link-variant"}/>,
+          title: "Links",
+          tabBarIcon: (props) => (
+            <MaterialCommunityIcons {...props} name={"link-variant"} />
+          ),
         }}
       />
       <Tabs.Screen
         name="tags"
         options={{
-          title: 'Tags',
-          tabBarIcon: (props) => <MaterialCommunityIcons {...props} name={"tag"}/>,
+          title: "Tags",
+          tabBarIcon: (props) => (
+            <MaterialCommunityIcons
+              {...props}
+              name={props.focused ? "tag-multiple" : "tag-multiple-outline"}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="archive"
         options={{
-          title: 'Archive',
-          tabBarIcon: (props) => <MaterialCommunityIcons {...props} name={"archive"}/>,
+          title: "Archive",
+          tabBarIcon: (props) => (
+            <MaterialCommunityIcons
+              {...props}
+              name={props.focused ? "archive" : "archive-outline"}
+            />
+          ),
         }}
       />
     </Tabs>
